b54dd15638e871b270de58e328eae9ea547e66e8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-07-20 15:34:14 +0000

    Deploy FirefoxUX/icons to github.com/FirefoxUX/icons.git:gh-pages

---
+++ viewer/js/data.js
@@ -0,0 +1,270 @@
+var data = [
+  {
+    "name": "new",
+    "items": [
+      "check-16.svg",
+      "customize-16.svg",
+      "delete-16.svg",
+      "developer-16.svg",
+      "device-desktop-16.svg",
+      "device-mobile-16.svg",
+      "external-link-16.svg",
+      "folder-16.svg",
+      "formfill-16.svg",
+      "highlights-16.svg",
+      "library-16.svg",
+      "link-16.svg",
+      "more-16.svg",
+      "new-16.svg",
+      "open-in-new-16.svg",
+      "pin-12.svg",
+      "pin-16.svg",
+      "pinremove-16.svg",
+      "screenshot-16.svg",
+      "subitem-16.svg",
+      "topsites-16.svg"
+    ]
+  },
+  {
+    "name": "alert",
+    "items": [
+      "help-16.svg",
+      "info-16.svg",
+      "warning-16.svg"
+    ]
+  },
+  {
+    "name": "badges",
+    "items": [
+      "deprecated-update-failed.svg",
+      "deprecated-update-success.svg"
+    ]
+  },
+  {
+    "name": "branding",
+    "items": [
+      "deprecated-firefox.svg",
+      "deprecated-nightly.svg"
+    ]
+  },
+  {
+    "name": "content",
+    "items": [
+      "copy-16.svg",
+      "cut-16.svg",
+      "deprecated-character-encoding.svg",
+      "deprecated-zoom-in.svg",
+      "deprecated-zoom-out.svg",
+      "fullscreen-16-disabled.svg",
+      "fullscreen-16.svg",
+      "fullscreen-exit-16.svg",
+      "open-16.svg",
+      "paste-16.svg",
+      "print-16.svg",
+      "save-16.svg",
+      "search-16.svg"
+    ]
+  },
+  {
+    "name": "developer",
+    "items": [
+      "deprecated-add.svg",
+      "deprecated-app.svg",
+      "deprecated-clear.svg",
+      "deprecated-close.svg",
+      "deprecated-command-console.svg",
+      "deprecated-command-eyedropper.svg",
+      "deprecated-command-frames.svg",
+      "deprecated-command-measure.svg",
+      "deprecated-command-noautohide.svg",
+      "deprecated-command-paintflashing.svg",
+      "deprecated-command-pick.svg",
+      "deprecated-command-responsivemode.svg",
+      "deprecated-command-rulers.svg",
+      "deprecated-command-screenshot.svg",
+      "deprecated-commandline-icon.svg",
+      "deprecated-console-input.svg",
+      "deprecated-console-output.svg",
+      "deprecated-debugger-step-in.svg",
+      "deprecated-debugger-step-out.svg",
+      "deprecated-debugger-step-over.svg",
+      "deprecated-debugging-devices.svg",
+      "deprecated-debugging-workers.svg",
+      "deprecated-developer-panel.svg",
+      "deprecated-developer.svg",
+      "deprecated-diff.svg",
+      "deprecated-dir-close.svg",
+      "deprecated-dir-open.svg",
+      "deprecated-dock-bottom.svg",
+      "deprecated-dock-side.svg",
+      "deprecated-dock-undock.svg",
+      "deprecated-fast-forward.svg",
+      "deprecated-filter.svg",
+      "deprecated-geometry-editor.svg",
+      "deprecated-globe.svg",
+      "deprecated-import.svg",
+      "deprecated-item-toggle.svg",
+      "deprecated-pane-collapse.svg",
+      "deprecated-pane-expand.svg",
+      "deprecated-pause.svg",
+      "deprecated-play.svg",
+      "deprecated-power.svg",
+      "deprecated-promise-debugger.svg",
+      "deprecated-pseudo-class.svg",
+      "deprecated-rewind.svg",
+      "deprecated-rotate-viewport.svg",
+      "deprecated-search.svg",
+      "deprecated-tool-canvas.svg",
+      "deprecated-tool-debugger.svg",
+      "deprecated-tool-dom.svg",
+      "deprecated-tool-inspector.svg",
+      "deprecated-tool-memory.svg",
+      "deprecated-tool-network.svg",
+      "deprecated-tool-options.svg",
+      "deprecated-tool-profiler.svg",
+      "deprecated-tool-scratchpad.svg",
+      "deprecated-tool-shadereditor.svg",
+      "deprecated-tool-storage.svg",
+      "deprecated-tool-styleeditor.svg",
+      "deprecated-tool-webaudio.svg",
+      "deprecated-tool-webconsole.svg",
+      "deprecated-touch-events.svg",
+      "deprecated-webide.svg"
+    ]
+  },
+  {
+    "name": "general",
+    "items": [
+      "deprecated-settings.svg",
+      "extensions-16.svg",
+      "mail-16.svg",
+      "preferences-16-macos.svg",
+      "privatebrowsing-16.svg",
+      "quit-16.svg",
+      "share-16-macos.svg",
+      "share-16-windows.svg",
+      "sync-16.svg"
+    ]
+  },
+  {
+    "name": "media",
+    "items": [
+      "audio-16-mute.svg",
+      "audio-16.svg",
+      "deprecated-caption.svg",
+      "deprecated-cast.svg",
+      "deprecated-chapter.svg",
+      "deprecated-pause.svg",
+      "deprecated-rewind.svg",
+      "fullscreen-16-disabled.svg",
+      "fullscreen-16.svg",
+      "fullscreen-exit-16.svg",
+      "play-16.svg"
+    ]
+  },
+  {
+    "name": "navigation",
+    "items": [
+      "arrowhead-down-12.svg",
+      "arrowhead-down-16.svg",
+      "arrowhead-left-12.svg",
+      "arrowhead-left-16.svg",
+      "arrowhead-right-12.svg",
+      "arrowhead-right-16.svg",
+      "arrowhead-up-12.svg",
+      "arrowhead-up-16.svg",
+      "back-16.svg",
+      "close-16.svg",
+      "deprecated-go.svg",
+      "deprecated-tab-groups.svg",
+      "forget-16.svg",
+      "forward-16.svg",
+      "home-16.svg",
+      "menu-16.svg",
+      "overflow-16.svg",
+      "privatebrowsing-16.svg",
+      "refresh-16.svg",
+      "search-16.svg",
+      "sidebar-16.svg",
+      "stop-16.svg",
+      "tab-16.svg",
+      "tab-new-16.svg",
+      "window-16.svg",
+      "window-new-16.svg"
+    ]
+  },
+  {
+    "name": "permissions",
+    "items": [
+      "deprecated-permissions.svg",
+      "location-16-disabled.svg",
+      "location-16-mac-disabled.svg",
+      "location-16-mac.svg",
+      "location-16-win-disabled.svg",
+      "location-16-win.svg",
+      "location-16.svg",
+      "login-16.svg",
+      "microphone-16-disabled.svg",
+      "microphone-16.svg",
+      "mouse-pointer-16-disabled.svg",
+      "mouse-pointer-6.svg",
+      "notification-16-disabled.svg",
+      "notification-16.svg",
+      "plugin-16-disabled.svg",
+      "plugin-16.svg",
+      "popup-block-16.svg",
+      "screen-share-16-disabled.svg",
+      "screen-share-16.svg",
+      "store-data-16-disabled.svg",
+      "store-data-16.svg",
+      "video-recorder-16-disabled.svg",
+      "video-recorder-16.svg"
+    ]
+  },
+  {
+    "name": "places",
+    "items": [
+      "bookmark-16-filled.svg",
+      "bookmark-16-Outline.svg",
+      "bookmark-remove-16.svg",
+      "bookmarksmenu-16.svg",
+      "deprecated-import-export.svg",
+      "deprecated-list.svg",
+      "deprecated-organize.svg",
+      "deprecated-search-history.svg",
+      "download-16.svg",
+      "history-16.svg",
+      "subscribe-16.svg"
+    ]
+  },
+  {
+    "name": "reader",
+    "items": [
+      "deprecated-exit.svg",
+      "deprecated-narrate-faster.svg",
+      "deprecated-narrate-slower.svg",
+      "deprecated-narrate.svg",
+      "deprecated-type-controls-serif.svg",
+      "deprecated-type-controls.svg",
+      "pocket-16.svg",
+      "pocket-list-16.svg",
+      "readermode-16.svg"
+    ]
+  },
+  {
+    "name": "security",
+    "items": [
+      "deprecated-identity-mixed-active-blocked.svg",
+      "deprecated-identity-mixed-active-loaded.svg",
+      "deprecated-identity-mixed-passive-loaded.svg",
+      "deprecated-identity-not-secure.svg",
+      "deprecated-identity-secure.svg",
+      "identity-icon-hover.svg",
+      "identity-icon-notice-hover.svg",
+      "identity-icon-notice.svg",
+      "identity-icon.svg",
+      "tracking-protection-16-disabled.svg",
+      "tracking-protection-16.svg"
+    ]
+  }
+]
